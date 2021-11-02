74d58ebc6f7038c80619e746459846f3d9c4689d
CrimsonKurikara <gabriel_silva.2016@outlook.com>
2021-11-01 21:54:05 -0300

    path

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
-app.get("/", function (req, res) {
+const port = 3000; // Const para armanezar a porta do servidor
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+// Substituição de function por arrow function
+app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.listen(3000);
+app.get("/index", (req, res) => {
+    res.render("index"); // Nome do arquivo, o EJS já busca dentro da pasta views.
+  })
+// Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
