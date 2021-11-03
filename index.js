daf1957020a17853375dbb4d9facf3051a607020
CrimsonKurikara <gabriel_silva.2016@outlook.com>
2021-11-03 13:15:22 -0300

    changed content

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.get("/index", (req, res) => {
-    res.render("index"); // Nome do arquivo, o EJS já busca dentro da pasta views.
+  const devList = ["Backend", "Frontend", "Fullstack"];
+  const analyticsList = ["Engenharia de dados", "Ciência de dados"];
+  res.render("index", { nome: "Gabriel", devList: devList, analyticsList: analyticsList}); // Nome do arquivo, o EJS já busca dentro da pasta views.
   })
 // Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
